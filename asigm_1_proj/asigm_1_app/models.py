6fd1194b7f258cfb807bbf62f7718cff3f608cff
PhuongNguyen209 <phuongktqs93@gmail.com>
2022-02-28 18:22:39 +0700

    Assignment1

--- asigm_1_proj/asigm_1_app/models.py
+++ asigm_1_proj/asigm_1_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField(null=True)
+    address = models.CharField(max_length=100)
+    mobile_number = models.IntegerField(null=True)
+    def __str__(self):
+        return self.name
